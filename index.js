e8e0706f1f9793508c8e36cc064e52353377c402
vuquangphong <thapquanviet@gmail.com>
2021-11-29 23:08:13 +0700

    just comment something

--- index.js
+++ index.js
@@ -8,12 +8,16 @@ require("dotenv").config();
 db.connect();
 
 // MIDDLEWARE
-
 // middleware nay dung de lay duoc du lieu nhap tu req.body
+
+
 // json() dung de parse req object thanh json object
 app.use(express.json());
+
+
 // urlencoded() dung de parse req object co the thanh nested object neu extended == true
 app.use(express.urlencoded({ extended: false }));
+
 
 app.use(cors());
 // middleware duoi day co the su dung thay cho middleware built-in cors
@@ -21,16 +25,19 @@ app.use(cors());
 //     res.header("Access-Control-Allow-Origin", "*");
 //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 //     next();
-//   });
+// });
+
 
 // cookie-parser la 1 middleware dung de phan tich cu phap cookie
 app.use(cookieParser());
+
 
 // ROUTES
 app.use("/api", require("./routes/authRouter"));
 app.use("/user", require("./routes/userRouter"));
 app.use("/unit", require("./routes/unitRouter"));
 app.use("/citizen", require("./routes/citizenRouter"));
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
